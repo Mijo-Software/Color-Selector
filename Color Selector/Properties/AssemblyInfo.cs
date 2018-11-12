5f3722e53067e29a2879269e4da0205e4529b0dd
Michael Johne <mjohne@users.noreply.github.com>
2018-11-12 15:16:04 +0100

    Add files via upload

--- Color Selector/Properties/AssemblyInfo.cs
+++ Color Selector/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("Color Selector")]
-[assembly: AssemblyDescription("Mit 'Color Selector' können alle RGB-Fraben erzeugt und bearbeitet werden.\n\n Dieses Entwicklungstool eignet sich besonders für Webdesigner und Programmierer.")]
+[assembly: AssemblyDescription("Mit 'Color Selector' können alle RGB-Farben erzeugt und bearbeitet werden.\n\n Dieses Entwicklungstool eignet sich besonders für Webdesigner und Programmierer.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mijo Soft")]
 [assembly: AssemblyProduct("Color Selector")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.4")]
-[assembly: AssemblyFileVersion("1.1.1.4")]
+[assembly: AssemblyVersion("1.2.0.5")]
+[assembly: AssemblyFileVersion("1.2.0.5")]
